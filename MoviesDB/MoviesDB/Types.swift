40bb974ec7ca40cd1f6525dbc3c192cbd98b6d0a
Denis Silko <silkodenis@gmail.com>
2024-09-08 11:42:19 +0300

    Refactor

--- MoviesDB/MoviesDB/Types.swift
+++ MoviesDB/MoviesDB/Types.swift
@@ -1,13 +1,13 @@
 import Core
 import Combine
-import CombineReduxStore
+import CombineStore
 import NavigationCoordinator
 
 typealias Graph = Core.Graph
 typealias AppState = Core.AppState
 typealias AppAction = Core.AppAction
+typealias AppStore = CombineStore.Store<AppState, AppAction>
+typealias Feedback = CombineStore.Feedback<AppState, AppAction>
 typealias NoEffect = Empty<AppAction, Never>
-typealias AppStore = CombineReduxStore.Store<AppState, AppAction>
-typealias Feedback = CombineReduxStore.Feedback<AppState, AppAction>
 typealias Coordinator = NavigationCoordinator<Screen>
 typealias RootView = NavigationStackRootView<Screen>
